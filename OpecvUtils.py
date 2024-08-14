b5ef9903161f0f13d76af03d18147ead5f8e2930
o1 <empty@empty.com>
2024-08-14 10:09:36 +0800

    新增skimage进行structural_similarity灰度后的图片相似度对比

--- OpecvUtils.py
+++ OpecvUtils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import datetime
+from skimage.metrics import structural_similarity
 BASE_PATH = 'static/'
 
 
@@ -35,13 +36,55 @@ def compare_img(img1path: str, img2path: str):
     img2 = cv2.resize(img2, size, cv2.INTER_AREA)
     img1_gray_mat = cv2.cvtColor(img1, cv2.COLOR_BGRA2GRAY, cv2.CV_8UC3)
     img2_gray_mat = cv2.cvtColor(img2, cv2.COLOR_BGRA2GRAY, cv2.CV_8UC3)
-    img1_hist = cv2.calcHist(img1_gray_mat, [0], None, [255], [0, 255])
-    img2_hist = cv2.calcHist(img2_gray_mat, [0], None, [255], [0, 255])
-    cv2.imshow("img1_gray_mat", img1_gray_mat)
-    cv2.imshow("img2_gray_mat", img2_gray_mat)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow("img1_gray_mat", img1_gray_mat)
+    # cv2.imshow("img2_gray_mat", img2_gray_mat)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # img1_hist = cv2.calcHist(img1_gray_mat, [0], None, [255], [0, 255])
+    # img2_hist = cv2.calcHist(img2_gray_mat, [0], None, [255], [0, 255])
     # ssim = cv2.compareHist(img1_hist, img2_hist, cv2.HISTCMP_CORREL)
-    diff = cv2.absdiff(img1_gray_mat, img2_gray_mat)
-    mse = np.mean(diff * 2)
-    return mse
+
+    # diff = cv2.absdiff(img1_gray_mat, img2_gray_mat)
+    # ssim = np.mean(diff * 2)
+
+    # img_compare = cv2.compare(img1_gray_mat, img2_gray_mat, cv2.CMP_EQ)
+    # cv2.imshow("img_compare", img_compare)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # img1_hist = cv2.calcHist(img1_gray_mat, [0], None, [256], [0, 255])
+    # img2_hist = cv2.calcHist(img2_gray_mat, [0], None, [256], [0, 255])
+    # ssim = 0
+    # for i in range(len(img1_hist)):
+    #     if img1_hist[i] != img2_hist[i]:
+    #         ssim = ssim + (1 - (abs(img1_hist[i] - img2_hist[i]) / max(img1_hist, img2_hist)))
+    #     else:
+    #         ssim = ssim + 1
+    # ssim = ssim / len(img1_hist)
+
+    # sub_image1 = cv2.split(img1_gray_mat)
+    # sub_image2 = cv2.split(img2_gray_mat)
+    # ssim = 0
+    # for i1, i2 in zip(sub_image1, sub_image2):
+    #     ssim += cal2(i1, i2)
+    # ssim = ssim / 3
+
+    # ssim = np.sum((img1_gray_mat.astype("float") - img2_gray_mat.astype("float")) ** 2)
+    # ssim /= float(img1_gray_mat.shape[0] * img1_gray_mat.shape[1])
+
+    ssim, _ = structural_similarity(img1_gray_mat, img2_gray_mat, full=True)
+    return ssim
+
+
+def cal2(im1, im2):
+    img1_hist = cv2.calcHist(im1, [0], None, [256], [0, 255])
+    img2_hist = cv2.calcHist(im2, [0], None, [256], [0, 255])
+    ssim = 0
+    for i in range(len(img1_hist)):
+        if img1_hist[i] != img2_hist[i]:
+            ssim = ssim + (1 - (abs(img1_hist[i] - img2_hist[i]) / max(img1_hist, img2_hist)))
+        else:
+            ssim = ssim + 1
+    ssim = ssim / len(img1_hist)
+    return ssim
